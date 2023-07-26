1d85c88d9a1182cedd58335d60d7919699ea7095
Dragon2000v <shmatko064@gmail.com>
2023-07-26 20:57:21 +0300

    Добавьте файлы проекта.

---
+++ Perform CRUD Operations/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Perform_CRUD_Operations.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Perform CRUD Operations/Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Perform_CRUD_Operations.Models;
+
+namespace Perform_CRUD_Operations.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
