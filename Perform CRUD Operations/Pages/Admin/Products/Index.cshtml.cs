fa3a4a2c8125531139abd571e31082111a4dce7e
Dragon2000v <shmatko064@gmail.com>
2023-07-30 14:18:46 +0300

    Add pagination functionality

--- Perform CRUD Operations/Pages/Admin/Products/Index.cshtml.cs
+++ Perform CRUD Operations/Pages/Admin/Products/Index.cshtml.cs
@@ -7,17 +7,38 @@ namespace Perform_CRUD_Operations.Pages.Admin.Products
 {
     public class IndexModel : PageModel
     {
-        private readonly ApplicationDbContext context;
+         private readonly ApplicationDbContext context;
 
-        public List<Product> Products { get; set; } = new List<Product>();
+         //pagination functionality
+         public int pageIndex = 1;
+         public int totalPages = 0;
+         private readonly int pageSize = 5;
 
-        public IndexModel(ApplicationDbContext context)
-        {
-            this.context = context;
-        }
-        public void OnGet()
-        {
-            Products = context.Products.OrderByDescending(p => p.Id).ToList();
-        }
+         public List<Product> Products { get; set; } = new List<Product>();
+
+         public IndexModel(ApplicationDbContext context)
+         {
+             this.context = context;
+         }
+         public void OnGet(int? pageIndex)
+         {
+             IQueryable<Product> query = context.Products;
+             query = query.OrderByDescending(p => p.Id);
+
+             // pagination functionality
+             if (pageIndex == null || pageIndex < 1)
+             {
+                 pageIndex = 1;
+             }
+
+             this.pageIndex = (int) pageIndex;
+
+             decimal count = query.Count();
+             totalPages = (int)Math.Ceiling(count / pageSize);
+             query = query.Skip((this.pageIndex - 1) * pageSize).Take(pageSize);
+
+             Products = query.ToList();           
+         }
+       
     }
 }
