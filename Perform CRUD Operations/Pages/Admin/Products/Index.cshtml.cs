305bde3312ddfd91e02cfcae042521ac8d8dc47f
Dragon2000v <shmatko064@gmail.com>
2023-07-30 15:06:16 +0300

    Add sort functionality

--- Perform CRUD Operations/Pages/Admin/Products/Index.cshtml.cs
+++ Perform CRUD Operations/Pages/Admin/Products/Index.cshtml.cs
@@ -18,13 +18,17 @@ namespace Perform_CRUD_Operations.Pages.Admin.Products
         //search functionality
         public string search = "";
 
+        //sort functionality
+        public string column = "Id";
+        public string orderBy = "desc";
+
         public List<Product> Products { get; set; } = new List<Product>();
 
          public IndexModel(ApplicationDbContext context)
          {
              this.context = context;
          }
-         public void OnGet(int? pageIndex, string? search)
+         public void OnGet(int? pageIndex, string? search, string? column, string? orderBy)
          {
              IQueryable<Product> query = context.Products;
 
@@ -35,10 +39,92 @@ namespace Perform_CRUD_Operations.Pages.Admin.Products
                 query = query.Where(p => p.Name.Contains(search) || p.Brand.Contains(search));
              }
 
-            query = query.OrderByDescending(p => p.Id);
+            //sort functionality
+            string[] validColumns = { "Id", "Name", "Brand", "Category", "Price", "CreateAt" };
+            string[] validOrderBy = { "desc", "asc" };
 
-             // pagination functionality
-             if (pageIndex == null || pageIndex < 1)
+            if(!validColumns.Contains(column))
+            {
+                column = "Id";
+            }
+
+            if(!validOrderBy.Contains(orderBy))
+            {  
+                orderBy = "desc"; 
+            }
+
+            this.column = column;
+            this.orderBy = orderBy;
+
+            if(column == "Name")
+            {
+                if(orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Name);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Name);
+                }
+            }else if (column == "Brand")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Brand);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Brand);
+                }
+            }else if (column == "Category")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Category);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Category);
+                }
+            }
+            else if (column == "Price")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Price);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Price);
+                }
+            }
+            else if (column == "CreatedAt")
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.CreatedAt);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.CreatedAt);
+                }
+            }
+            else
+            {
+                if (orderBy == "asc")
+                {
+                    query = query.OrderBy(p => p.Id);
+                }
+                else
+                {
+                    query = query.OrderByDescending(p => p.Id);
+                }
+            }
+
+            // query = query.OrderByDescending(p => p.Id);
+
+            // pagination functionality
+            if (pageIndex == null || pageIndex < 1)
              {
                  pageIndex = 1;
              }
