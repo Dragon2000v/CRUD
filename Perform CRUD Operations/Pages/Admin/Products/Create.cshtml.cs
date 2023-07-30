8ef37f340b1c2e16b354cb36f16bf0b54494d3d1
Dragon2000v <shmatko064@gmail.com>
2023-07-30 13:08:02 +0300

    Implemented the functionality of adding a product, updating a product, removing a product

--- Perform CRUD Operations/Pages/Admin/Products/Create.cshtml.cs
+++ Perform CRUD Operations/Pages/Admin/Products/Create.cshtml.cs
@@ -39,8 +39,29 @@ namespace Perform_CRUD_Operations.Pages.Admin.Products
             }
 
             //save the image file
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(ProductDto.ImageFile!.FileName);
+
+            string imageFullPath = environment.WebRootPath + "/product/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                ProductDto.ImageFile.CopyTo(stream);
+            }
 
             //save the new product in the database
+            Product product = new Product()
+            {
+                Name = ProductDto.Name,
+                Brand = ProductDto.Brand,
+                Category = ProductDto.Category,
+                Price = ProductDto.Price,
+                Description = ProductDto.Description ?? "",
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now,
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
 
             //clear the form
             ProductDto.Name = "";
@@ -53,6 +74,8 @@ namespace Perform_CRUD_Operations.Pages.Admin.Products
             ModelState.Clear();
 
             successMessage = "Product created successfully";
+
+            Response.Redirect("/Admin/Products/Index");
         }
 
     }
---
+++ Perform CRUD Operations/Pages/Admin/Products/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Perform_CRUD_Operations.Services;
+
+namespace Perform_CRUD_Operations.Pages.Admin.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IWebHostEnvironment environment;
+        private readonly ApplicationDbContext context;
+
+        public DeleteModel(IWebHostEnvironment environment, ApplicationDbContext context)
+        {
+            this.environment = environment;
+            this.context = context;
+        }
+        public void OnGet(int? id)
+        {
+            if (id == null)
+            {
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            string imageFullPath = environment.WebRootPath + "/product/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+
+            context.Products.Remove(product);
+            context.SaveChanges();
+
+            Response.Redirect("/Admin/Products/Index");
+        }
+    }
+}
